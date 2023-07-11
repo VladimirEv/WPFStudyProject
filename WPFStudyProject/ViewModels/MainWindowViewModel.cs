707957c2f9b432289247e1a86fc0438e2383d406
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-07-11 18:42:32 +0300

    Test work the model of view of the main window

--- WPFStudyProject/ViewModels/MainWindowViewModel.cs
+++ WPFStudyProject/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WPFStudyProject.ViewModels.Base;
 
 namespace WPFStudyProject.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModelBase
     {
+        #region
+        private string _title = "Statistics analysis";
+
+        public string Title
+        {
+            get => _title;
+            // set
+            // {
+            //if(Equals(_title, value))return;
+            // _title = value;
+            // OnPropertyChanged();
+
+            // Set(ref _title, value);
+            // }
+            set=>Set(ref _title, value);
+        }
+        #endregion
     }
 }
