a2cc1b1cb24ca63c176c0ff987fbf693f5bb6fc0
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-07-12 17:37:00 +0300

    Added new Property "Status"

--- WPFStudyProject/ViewModels/MainWindowViewModel.cs
+++ WPFStudyProject/ViewModels/MainWindowViewModel.cs
@@ -21,5 +21,24 @@ namespace WPFStudyProject.ViewModels
             set=>Set(ref _title, value);
         }
         #endregion
+
+        #region
+        private string _status = "Ready !";
+
+        public string Status
+        {
+            get => _status;
+            // set
+            // {
+            //if(Equals(_status, value))return;
+            // _status = value;
+            // OnPropertyChanged();
+
+            // Set(ref _status, value);
+            // }
+            set => Set(ref _status, value);
+        }
+        #endregion
+
     }
 }
