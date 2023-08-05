4fd7727c1b847cd3a719e2be33d3f3427971cdec
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-08-05 09:15:54 +0300

    Main class of our App

---
+++ WPFStudyProject/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WPFStudyProject.Infrastructure.Commands.Base;
+
+namespace WPFStudyProject.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new Exception(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
