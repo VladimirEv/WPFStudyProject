e69c30bcd3cea3c7f3d7b554da6fe5f3ec4c10ed
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-08-06 20:14:18 +0300

    Icons to Interface

--- WPFStudyProject/Infrastructure/Commands/CloseApplicationCommand.cs
+++ WPFStudyProject/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WPFStudyProject.Infrastructure.Commands.Base;
 
 namespace WPFStudyProject.Infrastructure.Commands
 {
     internal class CloseApplicationCommand : Command
     {
-        public override bool CanExecute(object? parameter)
-        {
-            throw new NotImplementedException();
-        }
+        public override bool CanExecute(object? parameter) => true;
 
-        public override void Execute(object? parameter)
-        {
-            throw new NotImplementedException();
-        }
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
     }
 }
