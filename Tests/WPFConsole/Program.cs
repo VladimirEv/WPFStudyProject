984022a32684a413160c473f9cf117c090fc9a32
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-08-07 13:29:31 +0300

    Get need dates

--- Tests/WPFConsole/Program.cs
+++ Tests/WPFConsole/Program.cs
@@ -1,13 +1,52 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
 const string data_url = 
 @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-var client  = new HttpClient();
+static async Task<Stream> GetDateStream() //формирует поток данных
+{
+    var client = new HttpClient();
+    var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+    return await response.Content.ReadAsStreamAsync();
+}
 
-var response = client.GetAsync(data_url).Result;
+static IEnumerable<string> GetDataLines() //разбивает поток данных на строки
+{
+    using var data_stream = GetDateStream().Result;
+    using var data_reader = new StreamReader(data_stream);
 
-var csr_str = response.Content.ReadAsStringAsync().Result;
+    while(!data_reader.EndOfStream)
+    {
+        var line = data_reader.ReadLine();
+        if (string.IsNullOrWhiteSpace(line)) continue;
+        yield return line;
+    }
+}
+
+static DateTime[] GetDates()=> GetDataLines()
+    .First()
+    .Split(',')
+    .Skip(4)
+    .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+    .ToArray();
+
+//var client  = new HttpClient();
+//var response = client.GetAsync(data_url).Result;
+//var csr_str = response.Content.ReadAsStringAsync().Result;
+//Console.ReadLine();
+
+//foreach (var data_line in GetDataLines())
+//{
+//    Console.WriteLine(data_line);
+//    Console.ReadLine();
+//}
+
+var dates = GetDates();
+
+Console.WriteLine(string.Join("\r\n", dates));
 
 Console.ReadLine();
