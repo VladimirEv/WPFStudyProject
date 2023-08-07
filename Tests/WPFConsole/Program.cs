b29b3653a3e42437c0e6926da545903ec112bc65
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-08-07 12:55:55 +0300

    Request to server and get datas.

--- Tests/WPFConsole/Program.cs
+++ Tests/WPFConsole/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+
+const string data_url = 
+@"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+var client  = new HttpClient();
+
+var response = client.GetAsync(data_url).Result;
+
+var csr_str = response.Content.ReadAsStringAsync().Result;
+
+Console.ReadLine();
